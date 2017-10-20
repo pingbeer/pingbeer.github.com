84cc1746de62886f1a94f87a6e74c0f99d6acae7
ljp <ljp@ljp>
2017-10-20 17:38:46 +0800

    project2加上图片展览

--- javascripts/common.js
+++ javascripts/common.js
@@ -3,34 +3,34 @@ function firstAccess(){
     if(location.hash.indexOf('hevb') != -1){return false;}
     var checkFirstAccess = $('#checkFirstAccess').val();
     if (checkFirstAccess > 0) {
-        $.ajax({
-            type: "GET",
-            datatype: "json",
-            url: '../../api/isFirstAccess',
-            data:{id: checkFirstAccess},
-            success: function (data) {
-                if (data.status == 1) {
-                    if (data.total > 0) {
-                        $(".hevbalert .hevbcon").text(data.descrip+'+'+data.total);
-                        var str = String(data.total),len = str.length,html = '';
-                        for (var i = 0;i < len ;i++) {
-                            html += '<img src="../../images/hevb/'+str[i]+'.png">';
-                        }
-                        $(".hevbalert .hevbcoin").html(html);
-                        setTimeout(function(){
-                            $(".hevbalert").show().animate({opacity:1},'slow').click(function(){
-                                window.location.href = '../../hevbComplete/complete';
-                            });
-                        },1000);
-                        setTimeout(function(){
-                            $(".hevbalert").animate({opacity:0},'slow').hide("slow");
-                        },3000);
-                        location.hash = 'hevb';
-                    }
-                    delete data;
-                }
-            }
-        });
+        // $.ajax({
+        //     type: "GET",
+        //     datatype: "json",
+        //     url: '../../api/isFirstAccess',
+        //     data:{id: checkFirstAccess},
+        //     success: function (data) {
+        //         if (data.status == 1) {
+        //             if (data.total > 0) {
+        //                 $(".hevbalert .hevbcon").text(data.descrip+'+'+data.total);
+        //                 var str = String(data.total),len = str.length,html = '';
+        //                 for (var i = 0;i < len ;i++) {
+        //                     html += '<img src="../../images/hevb/'+str[i]+'.png">';
+        //                 }
+        //                 $(".hevbalert .hevbcoin").html(html);
+        //                 setTimeout(function(){
+        //                     $(".hevbalert").show().animate({opacity:1},'slow').click(function(){
+        //                         window.location.href = '../../hevbComplete/complete';
+        //                     });
+        //                 },1000);
+        //                 setTimeout(function(){
+        //                     $(".hevbalert").animate({opacity:0},'slow').hide("slow");
+        //                 },3000);
+        //                 location.hash = 'hevb';
+        //             }
+        //             delete data;
+        //         }
+        //     }
+        // });
     }
 }/*daiem add*/
 

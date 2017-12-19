96ef450298287ae304253a47484fbf8d0665c1d9
ljp <ljp@ljp>
2017-12-19 17:06:09 +0800

    ''

--- agCli/src/app/app.module.ts
+++ agCli/src/app/app.module.ts
@@ -1,18 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { RouterModule } from '@angular/router'
+import { AppComponent } from './app.component'
 
-
-import { AppComponent } from './app.component';
-
-
+import { ElModule,ExAppComponent } from 'element-angular'
+import 'element-angular/theme/index.css';
 @NgModule({
   declarations: [
-    AppComponent
+    ExAppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ElModule.forRoot()
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ExAppComponent]
 })
 export class AppModule { }

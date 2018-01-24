4c54274f506f705fb8525f4eb5fea86420a73c34
ljp <ljp@ljp>
2018-01-24 16:05:53 +0800

    'zhengli'

--- 活动页面/fiddler自动匹配url.js
+++
@@ -1,2 +0,0 @@
-REGEX:http://127.0.0.1/jquery..*js
-REGEX:^http://campaign.e-pointchina.com.cn/seckill2/weekseckill/index.html?appId=weekseckill&sectionNo=N011&userId=0003001c55207232&platform=wx*$
--- 活动页面/minsheng图片地址.js
+++
@@ -1,4 +0,0 @@
-
-http://promotion.creditcard.cmbc.com.cn/picture/30736activityimage8066120865.jpg
-
-43246activityimage3512816689.jpg
